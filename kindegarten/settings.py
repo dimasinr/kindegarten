a59ffaca28a442c7c0d57e10673d74c774314bfc
dimas <dimas.indra773@gmail.com>
2026-06-30 00:29:57 +0700

    dasda

--- kindegarten/settings.py
+++ kindegarten/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%bv3z1jhwy+0w&%j#71i#r=ir##!#jp+$xk4ib9n5-qe#funz6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 import os
 MEDIA_URL = '/media/'
